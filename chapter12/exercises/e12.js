04908ca02dd07108f363c673e481fd2067134550
Mark <138724906+mlenda000@users.noreply.github.com>
2023-07-12 11:49:06 -0400

    updated 12 exercise

--- chapter12/exercises/e12.js
+++ chapter12/exercises/e12.js
@@ -34,12 +34,12 @@ let superTardigrade= {
 };
 
 // After you have created the other object literals, add the astronautID property to each one.
-let i = 1;
-superChimpOne["astronautID"] = i++;
-salamander["astronautID"] = i++;
-superChimpTwo["astronautID"] = i++;
-superDog["astronautID"] = i++;
-superTardigrade["astronautID"] = i++;
+let id = 1;
+superChimpOne["astronautID"] = id++;
+salamander["astronautID"] = id++;
+superChimpTwo["astronautID"] = id++;
+superDog["astronautID"] = id++;
+superTardigrade["astronautID"] = id++;
 
 superChimpOne["move"] = Math.round(Math.random()*10);
 salamander["move"] = Math.round(Math.random()*10);
@@ -48,8 +48,35 @@ superDog["move"] = Math.round(Math.random()*10);
 superTardigrade["move"] = Math.round(Math.random()*10);
 
 // Create an array to hold the animal objects.
-let arr = [superChimpOne,salamander,superChimpTwo,superDog,superTardigrade];
+let crew = [superChimpOne,salamander,superChimpTwo,superDog,superTardigrade];
 
 // Print out the relevant information about each animal.
-console.log(arr);
+function relevantInformation(animal){
+    let tempArr = [];
+    for (let i = 0; i < animal.length; i++){
+    tempArr.push(`${animal[i].name} is a ${animal[i].species}. They are ${animal[i].age} years old and ${animal[i].mass} kilograms. Their ID is ${animal[i].astronautID}.`);
+    }
+    return tempArr;
+}
+
+console.log(relevantInformation(crew));
 // Start an animal race!
+
+
+function race(animals){
+    let steps;
+    let turns;
+    let newArr = []
+    for (let i = 0; i < animals.length; i++){
+        turns = 0;
+        steps = 0;
+        while (steps < 20){
+            steps += animals[i].move
+            turns++
+        }
+        newArr.push(`${animals[i].name} took ${turns} turns to take 20 steps.`)
+    }
+    return newArr
+}
+
+console.log(race(crew));
