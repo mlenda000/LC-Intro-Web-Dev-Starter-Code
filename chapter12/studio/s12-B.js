7a3822ef08d24d217fd8867bd9a3a958ff1aa8b6
Mark <138724906+mlenda000@users.noreply.github.com>
2023-07-13 10:46:53 -0400

    finished studio 12

--- chapter12/studio/s12-B.js
+++ chapter12/studio/s12-B.js
@@ -1,10 +1,50 @@
-// 12.8.3. Bonus Missions
+//Instead of randomly selecting a crew member for the spacewalk, have your program select the animal with the smallest oxygen consumption.
+function selectMinEntry(a){
+  let mass = [];
+  let totalMass =[];
+  let temp = 0;
+  for (let i=0;i<a.length;i++){
+    mass.push(a[i]['mass'])
+  
+  }
+  let sortedArray = mass.sort(function(a, b){return a-b});
+  totalMass =sortedArray.splice(0,3)
+    return totalMass;
+}
+
+function buildCrewArray(weight,candidate){
+  let tempArr = [];
+    for(let i = 0; i < candidate.length;i++){
+      for (let j=0;j<weight.length;j++){
+        if (weight[j] === candidate[i].mass) {
+        tempArr.push(candidate[i])[0]
+        }
+      }
+    }
+return tempArr
+}
 
 // Code your crewMass function here:
-
-
-// Code your fuelRequired function here:
-
+function crewMass(candidateMass){
+  let totalMass = 0;
+    for (let i = 0; i < candidateMass.length; i++){
+    totalMass += candidateMass[i].mass
+  }
+    return Math.round(totalMass*10)/10;
+  }
+  
+  // Code your fuelRequired function here:
+  function fuelRequired(candidate){
+    let totalFuel = 75000;
+    for (let i=0; i < candidate.length; i++){
+      if (candidate[i].species=== 'dog' || candidate[i].species === 'cat'){
+        totalFuel += 200;
+      }else{
+        totalFuel += 100;
+      }
+    }
+    return Math.ceil(totalFuel);
+  }
 
 // The pre-selected crew is in the array at the end of this file.
 // Feel free to add, remove, or switch crew members as you see fit.
@@ -53,4 +93,6 @@ let candidateA = {
   };
   
   let crew = [candidateB,candidateD,candidateF];
-  
+  let allCandidates = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF]
+
+  console.log(buildCrewArray(selectMinEntry(allCandidates),allCandidates))
--- chapter12/studio/tempCodeRunnerFile.js
+++ chapter12/studio/tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-console.log(animals, randomIds);
+for (let j = 0; j < 3; j++){
+  //   mass.push(a.splice([Math.round(Math.min)],1)[0]);
+  //   }
+  //   return mass;
